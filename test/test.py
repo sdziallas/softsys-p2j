abb40e6e26c4c2933cce53c63fd90a19693fee55
Sebastian Dziallas <sebastian@sdziallas.com>
2012-11-20 21:31:39 -0500

    update test.py with more logic to automatically compile java

--- test/test.py
+++ test/test.py
@@ -7,15 +7,16 @@ import os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import astor
 
+import subprocess
+
 def CheckArgs():
 
     func = dir(test_func)
     check = 0
 
-    print "List of available functions: " + str(func)
-
     if len(sys.argv) != 2:
         print "Please specify exactly one function as argument."
+        print "List of available functions: " + str(func)
         sys.exit()
 
     for i in func:
@@ -23,7 +24,8 @@ def CheckArgs():
             check += 1
 
     if check == 0:
-        print "Pick a function from the list above."
+        print "Pick a function from the list below."
+        print "List of available functions: " + str(func)
         sys.exit()
 
     func_name = sys.argv[1]
@@ -36,9 +38,20 @@ def CallAstor(func_name):
 
     func_ast = astor.codetoast(function)
 
-    print(astor.dump(func_ast))
-    print(function.__name__ + '\n')
-    print(astor.to_source(func_ast, function.__name__))
+    # make sure to call this from the test directory
+    # otherweise need to fix the path here
+    filename = str(function.__name__ +'.java')
+    f = open(filename, 'w')
+
+    # activate to print AST again
+    # print(astor.dump(func_ast))
+    print>>f, astor.to_source(func_ast, function.__name__)
+
+    return filename
 
 test = CheckArgs()
-CallAstor(test)
+name = CallAstor(test)
+
+# always remove existing files before committing
+subprocess.call(['javac', name])
+print "Successfully compiled code."
